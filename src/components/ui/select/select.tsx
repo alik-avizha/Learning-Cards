5a0d0d3960816e539377df02460fed2cafbd103f
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-19 21:55:53 +0300

    dev: fix select (no done)

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,6 +1,6 @@
 import { FC, forwardRef } from 'react'
 
-import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import { ChevronDownIcon } from '@radix-ui/react-icons'
 import * as Select from '@radix-ui/react-select'
 
 import { Typography } from '../typography'
@@ -9,33 +9,31 @@ import s from './select.module.scss'
 
 export const SelectDemo = () => {
   return (
-    <div className={s.selectWrapper}>
-      <Select.Root>
-        <Select.Trigger className={s.selectTrigger}>
-          <Select.Value placeholder="Select-box" />
-          <Select.Icon className={s.selectIcon}>
+    <Select.Root>
+      <Select.Trigger className={s.selectTrigger}>
+        <Select.Value placeholder="Select-box" />
+        <Select.Icon className={s.selectIcon}>
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal className={s.selectPortal}>
+        <Select.Content className={s.selectContent}>
+          {/*<Select.ScrollUpButton className={s.selectScrollButton}>
+            <ChevronUpIcon />
+          </Select.ScrollUpButton>*/}
+          <Select.Viewport className={s.selectViewport}>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </Select.Viewport>
+          <Select.ScrollDownButton className={s.selectScrollButton}>
             <ChevronDownIcon />
-          </Select.Icon>
-        </Select.Trigger>
-        <Select.Portal>
-          <Select.Content className={s.selectContent}>
-            <Select.ScrollUpButton className={s.selectScrollButton}>
-              <ChevronUpIcon />
-            </Select.ScrollUpButton>
-            <Select.Viewport className={s.selectViewport}>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
-            </Select.Viewport>
-            <Select.ScrollDownButton className={s.selectScrollButton}>
-              <ChevronDownIcon />
-            </Select.ScrollDownButton>
-          </Select.Content>
-        </Select.Portal>
-      </Select.Root>
-    </div>
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
   )
 }
 
@@ -47,7 +45,7 @@ type SelectItemProps = {
 const SelectItem: FC<SelectItemProps> = forwardRef<HTMLDivElement | null, SelectItemProps>(
   ({ children, ...props }, forwardedRef) => {
     return (
-      <Select.Item {...props} ref={forwardedRef}>
+      <Select.Item className={s.selectItem} {...props} ref={forwardedRef}>
         <Select.ItemText>
           <Typography variant={'body1'}>{children}</Typography>
         </Select.ItemText>
