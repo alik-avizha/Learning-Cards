e071680989c75bf4604844c41bec4ed212355028
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-19 17:36:38 +0300

    fix: radioGroupUI

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,32 +1,41 @@
+import { FC } from 'react'
+
 import * as RadioGroup from '@radix-ui/react-radio-group'
+
+import { Typography } from '../typography'
 
 import s from './radioGroup.module.scss'
 
-export const RadioGroupDemo = () => (
+type RadioGroupProps = {
+  isDisabled?: boolean
+}
+export const RadioGroupDemo: FC<RadioGroupProps> = ({ isDisabled = false }) => (
+  /*Не сделано контролируемое состояние радио, первый захардкоджен*/
+
   <form>
     <RadioGroup.Root className={s.radioGroupRoot} defaultValue="default" aria-label="View density">
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <RadioGroup.Item className={s.radioGroupItem} value="default" id="r1">
+      <div className={s.itemGroup}>
+        <RadioGroup.Item className={s.radioGroupItem} disabled value="default" id="r1">
           <RadioGroup.Indicator className={s.radioGroupIndicator} />
         </RadioGroup.Item>
-        <label className={s.label} htmlFor="r1">
-          Default
+        <label className={`${s.label} ${s.labelDisabled}`} htmlFor="r1">
+          <Typography variant={'body2'}>Default</Typography>
         </label>
       </div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div className={s.itemGroup}>
         <RadioGroup.Item className={s.radioGroupItem} value="comfortable" id="r2">
           <RadioGroup.Indicator className={s.radioGroupIndicator} />
         </RadioGroup.Item>
-        <label className={s.label} htmlFor="r2">
-          Comfortable
+        <label className={`${s.label} ${isDisabled ? s.labelDisabled : ''}`} htmlFor="r2">
+          <Typography variant={'body2'}>Comfortable</Typography>
         </label>
       </div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div className={s.itemGroup}>
         <RadioGroup.Item className={s.radioGroupItem} value="compact" id="r3">
           <RadioGroup.Indicator className={s.radioGroupIndicator} />
         </RadioGroup.Item>
-        <label className={s.label} htmlFor="r3">
-          Compact
+        <label className={`${s.label} ${isDisabled ? s.labelDisabled : ''}`} htmlFor="r3">
+          <Typography variant={'body2'}>Compact</Typography>
         </label>
       </div>
     </RadioGroup.Root>
