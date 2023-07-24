100f7f85390ccd01ece43fecc79844482931d609
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-24 21:37:19 +0300

    feat: modal (no done)

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -13,7 +13,7 @@ export const Modal: FC<PropsType> = ({ isOpenModal, children }) => {
   let [isOpen, setIsOpen] = useState(isOpenModal)
 
   return (
-    <Dialog open={isOpen} onClose={setIsOpen} className={s.modalWrapper}>
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className={s.modalWrapper}>
       <Dialog.Panel className={s.dialog}>{children}</Dialog.Panel>
     </Dialog>
   )
