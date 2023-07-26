b8157600b7a137ae877c3be71f14ea507c4fd792
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-26 20:21:37 +0300

    fix: tabSwitcher

--- src/components/ui/tabSwitcher/tabSwitcher.stories.tsx
+++ src/components/ui/tabSwitcher/tabSwitcher.stories.tsx
@@ -6,9 +6,20 @@ const meta = {
   title: 'Components/TabSwitcher',
   component: TabSwitcher,
   tags: ['autodocs'],
+  argTypes: { onChangeCallback: { action: 'tabSwitcher changes' } },
 } satisfies Meta<typeof TabSwitcher>
+
+const tabsName = [
+  { name: 'Switcher', value: 'Button1', isDisabled: false },
+  { name: 'Switcher', value: 'Button2', isDisabled: false },
+  { name: 'Switcher', value: 'Button3', isDisabled: false },
+]
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const ShowTabSwitcher: Story = {}
+export const ShowTabSwitcher: Story = {
+  args: {
+    options: tabsName,
+  },
+}
--- src/components/ui/tabSwitcher/tabSwitcher.tsx
+++ src/components/ui/tabSwitcher/tabSwitcher.tsx
@@ -1,19 +1,21 @@
+import { FC } from 'react'
+
 import * as Tabs from '@radix-ui/react-tabs'
 
 import { Typography } from '../typography'
 
 import s from './tabSwitcher.module.scss'
-export const TabSwitcher = () => {
-  const tabsName = [
-    { name: 'Switcher', value: 'Button1', isDisabled: false },
-    { name: 'Switcher', value: 'Button2', isDisabled: false },
-    { name: 'Switcher', value: 'Button3', isDisabled: false },
-  ]
 
+type PropsType = {
+  classname?: string
+  options?: any[]
+  onChangeCallback: (value: any) => void
+}
+export const TabSwitcher: FC<PropsType> = ({ options, onChangeCallback }) => {
   return (
-    <Tabs.Root className={s.tabsRoot} defaultValue="tab1">
-      <Tabs.List className={s.tabsList} aria-label="Manage your account">
-        {tabsName.map((tab, index) => {
+    <Tabs.Root className={s.tabsRoot} onValueChange={onChangeCallback}>
+      <Tabs.List className={s.tabsList}>
+        {options?.map((tab, index) => {
           return (
             <Tabs.Trigger
               disabled={tab.isDisabled}
