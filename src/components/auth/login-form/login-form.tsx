4b2c9a60b65e06507cce525d01b81a06ab0bc61e
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-29 21:07:16 +0300

    feat: first chart

---
+++ src/components/auth/login-form/login-form.tsx
@@ -0,0 +1,64 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useController, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '../../ui/button'
+import { CheckboxDemo } from '../../ui/checkbox'
+import { TextField } from '../../ui/textfield'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  rememberMe: z.boolean().default(false),
+})
+
+type FormValues = z.infer<typeof loginSchema>
+
+export const LoginForm = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onSubmit',
+  })
+
+  const {
+    field: { value, onChange },
+  } = useController({
+    name: 'rememberMe',
+    control,
+    defaultValue: true,
+  })
+
+  console.log('errors: ', errors)
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        {...register('email')}
+        errorMessage={errors.email?.message}
+        label={'email'}
+        type={'default'}
+      />
+      <TextField
+        {...register('password')}
+        errorMessage={errors.password?.message}
+        label={'password'}
+        type={'default'}
+      />
+      <CheckboxDemo
+        variant={'withText'}
+        checkBoxText={'remember me'}
+        onChange={onChange}
+        checked={value}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  )
+}
