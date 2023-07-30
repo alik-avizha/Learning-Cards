ba81b06743af93ae2ea9fda35931b8749d155cda
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-30 21:39:51 +0300

    fix: card story

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from 'react'
 import s from './card.module.scss'
 type CardProps = {
   children: ReactNode
-  className: string
+  className?: string
 }
 
 export const Card: FC<CardProps> = ({ children, className }) => {
