c4e8c3089a2eb17cb0539cae00ae9fb4528f77ca
Alik_Avizha <aleksandravizha@icloud.com>
2023-08-07 15:20:32 +0300

    fix: add ReduxStoreProviderDecorator

--- src/common/utils/decorators.tsx
+++ src/common/utils/decorators.tsx
@@ -1,5 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+
+import { baseApi } from '../../services/base-api.ts'
+import { cardsSlice } from '../../services/cards/cards.slice.ts'
+import { deckSlice } from '../../services/decks/deck.slice.ts'
+
+export const storyBookStore = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    [deckSlice.name]: deckSlice.reducer,
+    [cardsSlice.name]: cardsSlice.reducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+})
 
 export const BrowserRouterDecorator = (storyFn: () => React.ReactNode) => {
   return <BrowserRouter>{storyFn()}</BrowserRouter>
+}
+
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
+  return <Provider store={storyBookStore}>{storyFn()}</Provider>
 }
